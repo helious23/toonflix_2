d80cb155dd4dd83b38b0e03d161526171f88ab90
Max16 <max16@naver.com>
2023-10-06 11:05:35 +0900

    6.16 Url Launcher

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/services/api_service.dart';
 import 'package:toonflix/utils/loading_indicator.dart';
+import 'package:toonflix/widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -114,49 +115,9 @@ class _DetailScreenState extends State<DetailScreen> {
                   return Column(
                     children: [
                       for (var episode in snapshot.data!)
-                        Container(
-                          margin: const EdgeInsets.only(
-                            bottom: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border(
-                              top: BorderSide(color: Colors.green.shade200),
-                              bottom: BorderSide(color: Colors.green.shade200),
-                              left: BorderSide(color: Colors.green.shade200),
-                              right: BorderSide(color: Colors.green.shade200),
-                            ),
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 1,
-                                offset: const Offset(3, 3),
-                                color: Colors.green.withOpacity(0.5),
-                              )
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 20,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  episode.title,
-                                  style: TextStyle(
-                                      color: Colors.green.shade400,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                Icon(
-                                  Icons.chevron_right_rounded,
-                                  color: Colors.green.shade400,
-                                ),
-                              ],
-                            ),
-                          ),
+                        Episode(
+                          episode: episode,
+                          webtoonId: widget.id,
                         ),
                     ],
                   );
